93c308670101e17c521d3ad24a8245992c7728dc
Damir_Safarov <91431110+DamirSafarov@users.noreply.github.com>
2021-10-16 19:42:03 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$('.doctor__slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow_left.png" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/arrow_right.png" alt=""></button>',
+    dots:true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+});
+
+$('.certificate__slider').slick({
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    dots:true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow_left.png" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/arrow_right.png" alt=""></button>',
+});
